69df772d2c392c70c8ca3a3df0e3048389bf3d63
WafflesTheDestroyer <tannerch@mymail.vcu.edu>
2017-05-05 17:55:31 -0400

    Add files via upload

--- database_project_trigger_beer.sql
+++ database_project_trigger_beer.sql
@@ -5,11 +5,11 @@ increment by 1
 nocycle;
 
 create or replace view Beers as
-select be.beer_id, be.Beer_name, be.type, be.ABV, be.IBU, be.Color, br.brewery_name from beer be join brewery br on (be.brewery_id = br.brewery_id);
+select be.beer_id, be.Beer_name, be.type, be.ABV, be.IBU, be.Color, br.brewery_name, br.brewery_ID from beer be join brewery br on (be.brewery_id = br.brewery_id);
 
 create or replace trigger New_Beer 
 instead of insert on Beers
 Begin
-insert into beer (Beer_ID, Beer_Name, Type, ABV, IBU, Color, Brewery_ID) values (Beer_ID_seq.nextval, :new.Beer_Name, :new.type, :new.ABV, :new.IBU, :new.Color, :new.Brewery_id);
+insert into beer (Beer_ID, Beer_Name, Type, ABV, IBU, Color, Brewery_ID) values (Beer_ID_seq.nextval, :new.Beer_Name, :new.type, :new.ABV, :new.IBU, :new.Color, :new.Brewery_ID);
 END;
 
